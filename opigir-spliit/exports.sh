48d506f75bfae788119417de9ef8887a578db842
Lukeo <opigir@users.noreply.github.com>
2025-05-25 19:26:10 -0700

    db password fix

--- opigir-spliit/exports.sh
+++ opigir-spliit/exports.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Generate secure password for database if not set
-export APP_PASSWORD="spliit"
+# Use a fixed password to ensure consistency
+export APP_PASSWORD="UmbrelSpliit2025!"
 
-# Default port
 export APP_PORT="3000"
-
-# Default timezone  
 export TZ="UTC"
